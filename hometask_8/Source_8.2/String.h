7de9c4bebced7a070075840d94776876f7e8a78d
Andrey Khodko <newKhodand@yandex.ru>
2017-11-22 00:17:45 +0300

    Second added

---
+++ hometask_8/Source_8.2/String.h
@@ -0,0 +1,22 @@
+#pragma once
+
+struct String
+{
+	char *data;
+	int size = 0;
+};
+
+bool areEqual(String &string1, String &string2);
+
+void input(String &string);
+void output(String &string);
+
+String clone(String &string);
+
+void cont(String &string, String &addition);
+// [first, last)
+String getSubstring(String &string, int first, int last);
+
+void clear(String &string);
+
+char *getCharArray(String &string);
