e2cc8fdfd6d0c3742c5ea6591afb714207d47014
Andrey Khodko <newKhodand@yandex.ru>
2018-02-19 19:16:59 +0300

    1.2 added

---
+++ semester_2/hometask_1/Source_1.2/Stack.java
@@ -0,0 +1,25 @@
+package com.group144.khodko;
+
+public class Stack {
+    private StackElement head;
+
+    Stack() { }
+
+    Stack(int value) {
+        head = new StackElement(value, null);
+    }
+
+    public void push(int value) {
+        head = new StackElement(value, head);
+    }
+
+    public int pop(){
+        int result = head.value;
+        head = head.next;
+        return result;
+    }
+
+    public int top() {
+        return head.value;
+    }
+}
---
+++ semester_2/hometask_1/Source_1.2/StackElement.java
@@ -0,0 +1,11 @@
+package com.group144.khodko;
+
+public class StackElement {
+    int value;
+    StackElement next;
+
+    StackElement(int value, StackElement next) {
+        this.value = value;
+        this.next = next;
+    }
+}
